e5cc72f6bb9f30f3683cca0b2549df040dd3410a
mreed19 <michael.reed315@gmail.com>
2016-07-08 09:54:32 -0400

    Update User Model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,24 @@
 var db = require('../config/db');
 
 var userSchema = db.Schema({
-  name: String,
-  username: String
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', function(next) {
+  this.updated_at = Date.now();
+  next();
 });
 
 var User = db.model('User', userSchema);
