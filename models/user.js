df12586ee453c0a326f78a1b578b37897bcb9052
mreed19 <michael.reed315@gmail.com>
2016-07-11 15:10:48 -0400

    Authenticate users

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var db = require('../config/db');
+var bcrypt = require('bcryptjs');
 
 var userSchema = db.Schema({
   name: {
@@ -30,7 +31,11 @@ userSchema.methods.toJSON = function() {
   delete user.passwordDigest;
   delete user.__v;
   return user;
-}
+};
+
+userSchema.methods.authenticate = function(password, callback) {
+  bcrypt.compare(password, this.passwordDigest, (err, isMatch) => callback(isMatch));
+};
 
 var User = db.model('User', userSchema);
 
