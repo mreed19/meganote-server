5056277947280bf1341c86849811fa8b2990f930
mreed19 <michael.reed315@gmail.com>
2016-07-11 14:14:44 -0400

    Update users

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -30,6 +30,31 @@ router.post('/', function (req, res) {
     });
 });
 
+// UPDATE a user
+router.put('/:id', (req, res) => {
+  User.findOne({
+    _id: req.params.id
+  })
+  .then(
+    user => {
+      if (user) {
+        user.name = req.body.user.name;
+        user.username = req.body.user.username;
+        user.save()
+        .then(
+          () => res.json({ user }),
+          () => res.status(422).json({ message: 'Unable to update user.' })
+        );
+      }
+      else {
+        res.status(404).json({
+          message: 'Could not find that user.'
+        });
+      }
+    }
+  );
+})
+
 module.exports = router;
 
 function passwordsPresent(payload) {
