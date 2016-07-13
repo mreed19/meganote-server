5fd498722943fadb0a281a0fee2713d84736d284
mreed19 <michael.reed315@gmail.com>
2016-07-13 13:00:18 -0400

    Add error message to handle when no user is submitted to the server on login

--- routes/session-routes.js
+++ routes/session-routes.js
@@ -4,40 +4,45 @@ var User = require('../models/user');
 
 // login
 router.post('/', (req, res) => {
-  User.findOne({
-    username: req.body.user.username
-  })
-  .then(
-    user => {
-      if (user) {
-        // user exists
-        user.authenticate(req.body.user.password, (isMatch) => {
-          if (isMatch) {
-            // correct password
-            var token = jwt.sign(
-              { _id: user._id },
-              process.env.JWT_SECRET,
-              {
-                expiresIn: 60*60*24
-              }
-            );
-            res.json({
-              user,
-              authToken: token
-            })
-          }
-          else {
-            // wrong password
-            res.status(401).json({ message: 'We were unable to log you in with those credentials.'});
-          }
-        });
+  if (req.body.user) {
+    User.findOne({
+      username: req.body.user.username
+    })
+    .then(
+      user => {
+        if (user) {
+          // user exists
+          user.authenticate(req.body.user.password, (isMatch) => {
+            if (isMatch) {
+              // correct password
+              var token = jwt.sign(
+                { _id: user._id },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: 60*60*24
+                }
+              );
+              res.json({
+                user,
+                authToken: token
+              })
+            }
+            else {
+              // wrong password
+              res.status(401).json({ message: 'We were unable to log you in with those credentials.'});
+            }
+          });
+        }
+        else {
+          // user does note exist
+          res.status(401).json({ message: 'We were unable to log you in with those credentials.'});
+        }
       }
-      else {
-        // user does note exist
-        res.status(401).json({ message: 'We were unable to log you in with those credentials.'});
-      }
-    }
-  )
+    );
+  }
+  else {
+    res.status(401).json({ message: 'We were unable to log you in with those credentials.'});
+  }
 });
 
 module.exports = router;
